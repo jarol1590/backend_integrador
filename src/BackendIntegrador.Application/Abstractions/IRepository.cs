f03e431693d28f5479b7727783653323ff26bca3
jarol1590 <jarol1590@gmail.com>
2026-05-30 16:21:11 -0500

    agego metodo FirstOrDefaultAsync a IRespository

--- src/BackendIntegrador.Application/Abstractions/IRepository.cs
+++ src/BackendIntegrador.Application/Abstractions/IRepository.cs
@@ -1,9 +1,14 @@
+using System.Linq.Expressions;
+
 namespace BackendIntegrador.Application.Abstractions;
 
 public interface IRepository<TEntity> where TEntity : class
 {
     Task<TEntity?> FindAsync(object?[]? keyValues, CancellationToken cancellationToken = default);
     Task<IReadOnlyList<TEntity>> GetAllAsync(CancellationToken cancellationToken = default);
+
+    Task<TEntity?> FirstOrDefaultAsync(Expression<Func<TEntity, bool>> predicate, CancellationToken cancellationToken = default);
+
     Task AddAsync(TEntity entity, CancellationToken cancellationToken = default);
     Task UpdateAsync(TEntity entity, CancellationToken cancellationToken = default);
     Task RemoveAsync(TEntity entity, CancellationToken cancellationToken = default);
